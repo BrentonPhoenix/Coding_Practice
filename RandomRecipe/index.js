8eac0815b2bf51488b453af5b1d1cf9e985c19b9
BrentonPhoenix <brentonphoenix@gmail.com>
2021-11-17 19:28:41 -0500

    fixed the fetch issue!

--- RandomRecipe/index.js
+++ RandomRecipe/index.js
@@ -1,17 +1,17 @@
 let fetchButton = document.getElementById("fetchR")
 let recipeList = document.getElementsByTagName("ul")
 let jsonResults;
-
+let title = document.getElementsByTagName('h1')
+let instructions = document.getElementsByTagName('p')
 
 
 fetchButton.addEventListener('click', ev=>{
     fetch("https://themealdb.com/api/json/v1/1/random.php")
-    .then( e =>{
-       jsonResults = e.json()
+    .then( e => e.json() )
+    .then(json =>{
+        jsonResults = json
+        console.log(jsonResults.meals[0])
     })
-    .then(json => console.log(jsonResults))
-    
-
 })
 
 // Recipe Name, Ingredients, Instructions and a picture
